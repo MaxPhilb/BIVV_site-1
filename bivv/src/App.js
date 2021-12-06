b20fc3cd14a29909a4339f92687cf1057e615a65
MaxPhilb <79253830+MaxPhilb@users.noreply.github.com>
2021-12-06 16:35:33 +0100

    main

--- bivv/src/App.js
+++ bivv/src/App.js
@@ -1,16 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TopBar from './topBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          test
-        </p>
-      </header>
+        <TopBar />
     </div>
   );
 }
---
+++ bivv/src/topBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './topBar.css';
+
+class TopBar extends  React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+       
+      };
+      
+      
+      this.handleChange=this.handleChange.bind(this);
+
+    }
+  
+   
+  
+  
+  
+    
+    handleChange(event){
+      
+     
+      
+    }
+   
+    render(){
+        return(
+          <div className="topBar">
+            topBar
+          </div>      
+                      
+                  
+                  
+              
+        );
+    }
+  }
+  export default TopBar;
